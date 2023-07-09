e29d3f1940d7bdcebf339e372b5e0dba260a4152
CarlosDelgadoG <carlos.delgado@ug.uchile.cl>
2023-07-09 12:45:56 -0400

    Update codigo_arboles.R

--- parte_I/codigo_arboles.R
+++ parte_I/codigo_arboles.R
@@ -46,22 +46,63 @@ predicciones= predict(modelo_arbol,testing(diabetes_split))%>%
 
 caret::confusionMatrix(table(predicciones$outcome,predicciones$.pred_class))
 # Accuracy: De todas las predicciones cuantas fueron correctas 
-# Senstividad: De las predicciones positivas cuantas fueron correctas
-# Especificidad: De las predicciones negativas cuantas fueron correctas
-
+# Senstividad: De todos los valores positivos cuantos fueron correctamente identificados
+# Espeficidad: De todos los valores negativos encontrados, cuantes fueron correctamente identificados
+# AUC: 0,5 indica que el modelo no es mejor que el azar
 
 # VALIDACION CRUZADA ------------------------------------------------------
 
 set.seed(2022)
 
-diab_fols <- vfold_cv(diab_train,10)
+diab_fold <- vfold_cv(diab_train,3)
 
 
 fits_cv= fit_resamples(tree_spec,
               outcome~mass,
-              resamples=diab_fols,
+              resamples=diab_fold,
               metrics = metric_set(accuracy,sensitivity,specificity))
 
 
 
 collect_metrics(fits_cv)
+
+
+
+# Grilla de parametros ----------------------------------------------------
+
+### Crear especificacion con hiperparametros "libres"
+arbol_untune <- decision_tree(tree_depth = tune(),
+                              min_n = tune())%>%
+                set_engine("rpart")%>%
+                set_mode("classification")
+
+
+grilla <- expand.grid(tree_depth=c(5,10,15),
+                      min_n=c(10,20,30))
+
+
+
+# Tune along the grid
+tune_results <- tune_grid(arbol_untune, 
+                          outcome~mass+pregnant,
+                          resamples = diab_fold,
+                          grid = grilla,
+                          metrics = metric_set(roc_auc, sens, spec))
+
+
+
+collect_metrics(tune_results) 
+
+
+
+
+autoplot(tune_results)
+
+
+mejor_modelo <- finalize_model(arbol_untune, select_best(tune_results))%>%
+                fit(outcome~mass+pregnant,
+                    diabetes)
+
+
+
+rpart.plot(mejor_modelo$fit)
